ce27d7a60caab4e5a887716fbadb8095570f9247
Kaamkiya <ziam3.1415@gmail.com>
2025-10-01 20:23:37 -0400

    feat: Solutions to 2024 CCC jr 1

---
+++ meeting3-10-02/solutions/solution.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int R, G, B;
+	scanf("%d\n%d\n%d", &R, &G, &B);
+	printf("%d\n", R*3 + G*4 + B*5);
+}
---
+++ meeting3-10-02/solutions/solution.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  int R, G, B;
+  std::cin >> R >> G >> B;
+  std::cout << R * 3 + G * 4 + B * 5 << std::endl;
+}
